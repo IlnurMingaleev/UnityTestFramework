36dbd4d2098fe3db838f0ee9893c41826caad59c
Ilnur-web <ilmingaleev@yandex.ru>
2022-08-20 16:30:11 +0300

    Now animation speed is regulated by joystick

--- UnityTestFramework/Assets/Scripts/PlayerController.cs
+++ UnityTestFramework/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,8 @@ public class PlayerController : MonoBehaviour
     public float _speed;
     public Movement movement;
     public Animator _animator;
-    public SpriteRenderer _spriteRenderer;  
+    public SpriteRenderer _spriteRenderer;
+    public Joystick joystick;
     private void Start()
     {
         movement = new Movement(_speed);
@@ -18,12 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
+        float horizontalInput = joystick.Horizontal;
+        float verticalInput = joystick.Vertical;
         float deltaTime = Time.deltaTime;
         if (Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput) < float.Epsilon) return;
         _animator.SetFloat("moveX", horizontalInput);
         _animator.SetFloat("moveY", verticalInput);
+        
         if (horizontalInput < 0)
         {
             _spriteRenderer.flipX = true;
@@ -41,18 +43,20 @@ public class PlayerController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        float lastMoveX = Input.GetAxisRaw("Horizontal");
-        float lastMoveY = Input.GetAxisRaw("Vertical");
+        float lastMoveX = joystick.Horizontal;
+        float lastMoveY = joystick.Vertical;
 
         if (lastMoveX != 0 || lastMoveY != 0)
         {
             _animator.SetBool("isWalking", true);
             _animator.SetFloat("lastMoveX", lastMoveX);
             _animator.SetFloat("lastMoveY", lastMoveY);
+            _animator.SetFloat("speed", Mathf.Max(Mathf.Abs(lastMoveX), Mathf.Abs(lastMoveY)));
+            
         }
         else 
         {
-            _animator.SetBool("isWaling", false);
+            _animator.SetBool("isWalking", false);
         }
 
     }
